68130da2acadb564f9b647de9f2e59a225b036d6
yusuke0729 <siroha323@gmail.com>
2023-12-23 18:15:21 +0900

    DB関連

--- prisma/seed/data/characters_users.ts
+++ prisma/seed/data/characters_users.ts
@@ -8,7 +8,19 @@ export const insertCharactersUsers = async () => {
 				user_id: '018b6fbd-72b5-7000-b31b-42e0a3825abf',
 				character_id: 5,
 				character_name: 'ちょこ',
-				nostalgicLevel: 0,
+				character_data: {
+					nostalgic_level: 30,
+					color: {
+						eye: '1.000, 0.000, 0.000, 0.00',
+						ear: '1.000, 0.000, 0.000, 0.000',
+						body: '1.000, 0.000, 0.000, 0.000',
+					},
+					customize: {
+						neck: 'bell',
+						head: 'hat',
+						face: 'glasses',
+					},
+				},
 			},
 		],
 	});
